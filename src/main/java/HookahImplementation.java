9eab6094b5b32bded8f934add53cf4374b43f78a
joanossoubita23 <joanossoubita@yahoo.fr>
2023-12-20 21:00:53 -0500

    little implementation

--- src/main/java/HookahImplementation.java
+++ src/main/java/HookahImplementation.java
@@ -1,2 +1,28 @@
-public class HookahImplementation {
+import java.util.List;
+
+public class HookahImplementation  implements HookahDao{
+    @Override
+    public void addHookah(Hookah hookah) {
+
+    }
+
+    @Override
+    public void UpdateHookah(Hookah hookah) {
+
+    }
+
+    @Override
+    public void deleteHookahByid(int id) {
+
+    }
+
+    @Override
+    public Hookah getHookahByid(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Hookah> getAllHookahs() {
+        return null;
+    }
 }
